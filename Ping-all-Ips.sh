ed2481772091d39a8f691cc257a4016894e2bc37
blueberry21295 <nguyentiennghi212952019@gmail.com>
2023-02-01 15:29:56 +0100

    Ping all IPs

---
+++ Ping-all-Ips.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+alive=0
+dead=0
+ip_prefix=`echo $(hostname -i) | cut -d "." -f1-3`
+ip=`echo $ip_prefix.{223..225}`
+echo $ip
+for i in $ip
+do 
+ if [ "`ping -c 1 $i`" ]
+   then 
+	let "alive = alive +1";echo "$i" >> list_ip_alive.txt
+   else
+        let "dead = dead +1";echo "$i" >> list_ip_dead.txt
+ fi
+done
+echo "le nombre des IPs en ligne est $alive"
+echo "le nombre des IPs offline est $dead"
